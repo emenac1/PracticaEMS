896a076bee3bfa6527baae9ee50651a4274136d2
emenac <e.menac@alumnos.upm.es>
2022-04-08 14:06:31 +0200

    inserts correctos

---
+++ GestionAsignaturas - BBDD/insertsCorrectos.sql
@@ -0,0 +1,7 @@
+INSERT INTO `gestionasignaturas`.`convocatoria` (`idCONVOCATORIA`, `actual`) VALUES ('ordinaria', b'1');
+INSERT INTO `gestionasignaturas`.`curso` (`Cod_CURSO`, `actual`) VALUES ('2016', b'1');
+INSERT INTO `gestionasignaturas`.`examen` (`Cod_Ex`) VALUES ('ordinaria2016');
+INSERT INTO `gestionasignaturas`.`grupo_clase` (`Cod_GC`) VALUES ('1');
+INSERT INTO `gestionasignaturas`.`grupo_clase` (`Cod_GC`) VALUES ('2');
+INSERT INTO `gestionasignaturas`.`practica` (`Cod_Practica`) VALUES ('ordinaria2016');
+INSERT INTO `gestionasignaturas`.`tipo_evaluacion` (`Ev_Continua`, `Peso_Ex`, `Peso_Pr`, `Peso_Ev_C`) VALUES ('1', '50', '25', '25');
